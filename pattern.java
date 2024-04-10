efe7b3f676e5f92f90bd8f61d7277d631a67ad03
saurabh <saurabh@gmail.com>
2024-04-10 11:10:44 +0530

    rm js

---
+++ pattern.java
@@ -0,0 +1,10 @@
+public class pattern {
+public static void main(String[] args) {
+    for(int i=0;i<7;i++){
+        for(int j=0;j<7;j++){
+            int x = 0;
+        }
+    }
+    
+}
+}
--- second.js
+++
@@ -1,3 +0,0 @@
-console.log(hi);
-console.log("sir");
-let a = 10;
